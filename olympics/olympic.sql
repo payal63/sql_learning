c9b3a36088bb21967a368d5cc34dc3e6ca2ca50b
Payal Dhameliya <payalnarola63@gmail.com>
2024-10-17 20:20:14 -0500

    new question

--- olympics/olympic.sql
+++ olympics/olympic.sql
@@ -12,3 +12,17 @@ select oh.*,ng.region
 from practice.olympics_history oh
 join practice.olympics_history_noc_regions ng on oh.noc = ng.noc)x
 group by games;
+
+with cte as(
+select count(distinct region) total,games from(
+select oh.*,ng.region
+from practice.olympics_history oh
+join practice.olympics_history_noc_regions ng on oh.noc = ng.noc)x
+group by games),
+cte2 as
+(
+select min(total)lower,max(total)upper
+from cte)
+select c.total,c.games
+from cte2 c2 join
+cte c where c2.lower=c.total or c2.upper=c.total;
