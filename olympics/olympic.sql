cbdb892f3edb6f3593b30310da63b0ea9215a95a
Payal Dhameliya <payalnarola63@gmail.com>
2024-10-17 19:51:16 -0500

    distinct game

--- olympics/olympic.sql
+++ olympics/olympic.sql
@@ -5,3 +5,10 @@ from olympics_history
 select distinct year,season,city
 from olympics_history
 order by year;
+
+
+select count(distinct region),games from(
+select oh.*,ng.region
+from practice.olympics_history oh
+join practice.olympics_history_noc_regions ng on oh.noc = ng.noc)x
+group by games;
